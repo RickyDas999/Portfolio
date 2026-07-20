5c8e816465627bf0fb7b290c69e956cc2e8fff30
Ricky Das <das.ricky999@gmail.com>
2026-07-20 17:39:54 -0400

    Add Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://RickyDas999.github.io',
   base: '/Portfolio',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
